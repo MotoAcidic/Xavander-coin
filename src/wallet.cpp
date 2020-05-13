872583780632dd1ecd181cb6d7eeb1d4a2ef26df
Travis <travisfinch01@gmail.com>
2020-05-13 10:35:51 -0400

    Fix Collateral messup

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1625,13 +1625,13 @@ void CWallet::AvailableCoins(vector<COutput>& vCoins, bool fOnlyConfirmed, const
                 if (nCoinType == ONLY_DENOMINATED) {
                     found = IsDenominatedAmount(pcoin->vout[i].nValue);
                 } else if (nCoinType == ONLY_NOT10000IFMN) {
-					found = !(fMasterNode && pcoin->vout[i].nValue == CollateralRequired() * COIN);
+					found = !(fMasterNode && pcoin->vout[i].nValue == GetCurrentCollateral() * COIN);
                 } else if (nCoinType == ONLY_NONDENOMINATED_NOT10000IFMN) {
                     if (IsCollateralAmount(pcoin->vout[i].nValue)) continue; // do not use collateral amounts
                     found = !IsDenominatedAmount(pcoin->vout[i].nValue);
-					if (found && fMasterNode) found = pcoin->vout[i].nValue != CollateralRequired() * COIN; // do not use Hot MN funds
+					if (found && fMasterNode) found = pcoin->vout[i].nValue != GetCurrentCollateral() * COIN; // do not use Hot MN funds
                 } else if (nCoinType == ONLY_10000) {
-					found = pcoin->vout[i].nValue == CollateralRequired() * COIN;
+					found = pcoin->vout[i].nValue == GetCurrentCollateral() * COIN;
 				} else {
                     found = true;
                 }
@@ -2096,8 +2096,7 @@ bool CWallet::SelectCoinsDark(CAmount nValueMin, CAmount nValueMax, std::vector<
         if (out.tx->vout[out.i].nValue < CENT) continue;
         //do not allow collaterals to be selected
         if (IsCollateralAmount(out.tx->vout[out.i].nValue)) continue;
-		if (fMasterNode && out.tx->vout[out.i].nValue == CollateralRequired() * COIN) continue;
-        //if (fMasterNode && out.tx->vout[out.i].nValue == 10000 * COIN) continue; //masternode input
+		if (fMasterNode && out.tx->vout[out.i].nValue == GetCurrentCollateral() * COIN) continue;
 
         if (nValueRet + out.tx->vout[out.i].nValue <= nValueMax) {
             CTxIn vin = CTxIn(out.tx->GetHash(), out.i);
